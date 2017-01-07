f2f74ce63f859c141c3f6dcc6feee5b6a3503347
Eugene1992 <deyneko55@gmail.com>
2017-01-08 00:10:01 +0200

    #17 Created base Spring app configuration.

---
+++ web/src/main/java/com/netcracker/config/AppInit.java
@@ -0,0 +1,30 @@
+package com.netcracker.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Application config.
+ */
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
---
+++ web/src/main/java/com/netcracker/config/MainController.java
@@ -0,0 +1,21 @@
+package com.netcracker.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Main page controller.
+ */
+@Controller
+@RequestMapping("/projects")
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+        model.addAttribute("message", "Spring 3 MVC - Hello World");
+        return "projects";
+    }
+}
