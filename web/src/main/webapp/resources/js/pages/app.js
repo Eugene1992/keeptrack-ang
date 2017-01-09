471aee294eb863028f360b8a229bc56cf6aaa94a
Eugene1992 <deyneko55@gmail.com>
2017-01-09 12:28:01 +0200

    #22 Added page routing.

--- web/src/main/webapp/resources/js/pages/app.js
+++ web/src/main/webapp/resources/js/pages/app.js
@@ -1,2 +1,11 @@
 
-var module = angular.module("mainApp", ["isteven-multi-select"]);
+var module = angular.module("mainApp", ["isteven-multi-select"])
+    .controller("MainCtrl", function ($scope) {
+
+    $scope.currentView = 'WEB-INF/pages/employees.html';
+
+    $scope.changeView = function (view) {
+        var path = 'WEB-INF/pages/' + view + '.html';
+        $scope.currentView = path;
+    }
+})
